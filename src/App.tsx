6f43c1966b5166a8a33d525f5349216dad574b24
Kyle Quilty <kquilty@gmail.com>
2025-11-27 11:57:54 -0500

    Added DieProps

--- src/App.tsx
+++ src/App.tsx
@@ -13,24 +13,28 @@ function App() {
 function Gameboard(){
     return (
         <div className='gameboard'>
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
+            <Die value={1} isHeld={false} />
+            <Die value={2} isHeld={false} />
+            <Die value={3} isHeld={false} />
+            <Die value={4} isHeld={false} />
+            <Die value={5} isHeld={false} />
+            <Die value={6} isHeld={false} />
+            <Die value={7} isHeld={false} />
+            <Die value={8} isHeld={false} />
+            <Die value={9} isHeld={false} />
         </div>
     )
 }
-function Die(){
+function Die(props: DieProps){
     return (
-        <div className='die'>
-            x
+        <div className={props.isHeld ? "die held" : "die"}>
+            {props.value}
         </div>
     )
+}
+interface DieProps {
+    value: number,
+    isHeld: boolean
 }
 
 export default App
