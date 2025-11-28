294ce32b033e22bf475f864811eea9f10d615274
Kyle Quilty <kquilty@gmail.com>
2025-11-28 14:36:48 -0500

    - added Win detection - added total rolls count to Win message

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,52 @@ import RollDiceButton from './components/RollDiceButton'
 
 function App() {
     const [dieObjects, setDieObjects] = useState<DieObject[]>([])
+    const [clickCount, setClickCount] = useState(0)
 
     // rollAllDiceFresh()
+    const isGameWon = getIsGameWon()
 
     function GetRandomDieNumber() {
         return Math.ceil(Math.random() * 6);
     }
 
-    function onRollDiceClick(){
-        rollAllDiceFresh()
+    function getIsGameWon(){
+        if(dieObjects.length === 0){
+            return false
+        }
+        const firstValue = dieObjects[0].value
+        for(const dieObject of dieObjects){
+            if(dieObject.value !== firstValue || !dieObject.isHeld){
+                return false
+            }
+        }
+        return true
     }
+
+    function onRollDiceClick(){
+        if(dieObjects.length === 0 || isGameWon){
+            rollAllDiceFresh()
+            setClickCount(1)
+        } else {
+            rollDice()
+            setClickCount(clickCount + 1)
+        }
+    }
+    
+    function rollDice(){
+        const newDieObjects = dieObjects.map(dieObject => {
+            if(dieObject.isHeld){
+                return dieObject
+            } else {
+                return {
+                    ...dieObject,
+                    value: GetRandomDieNumber()
+                }
+            }
+        })
+        setDieObjects(newDieObjects)
+    }
+    
     function rollAllDiceFresh(){
         const newDieObjects = []
         for(let i = 0; i < 9; i++){
@@ -40,11 +76,16 @@ function App() {
 
     return (
         <div className="App">
+
             <Gameboard 
                 dieObjects={dieObjects} 
                 onDieClick={handleDieClick}
             />
+
             <RollDiceButton onRollDiceClick={onRollDiceClick} />
+
+            {isGameWon && <div className='won-message'>You won in {clickCount} rolls!</div>}
+
         </div>
     )
 }
